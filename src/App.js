b6beeabc390824504a1ba005d12aab865ae7e068
James Mokua <jamieedgar02@gmail.com>
2022-05-07 19:55:47 +0300

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import './App.css'
 
 function App() {
   return (
     <div className="app">
-          <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Navbar/>
     </div>
   );
 }
